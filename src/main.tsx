1348fa5c6160d228378a6ebdc38edbf2c16ed919
HermannDotCom <85178448+HermannDotCom@users.noreply.github.com>
2025-10-19 06:24:41 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const allowedKeys = ['theme', 'sb-byuwnxrfnfkxtmegyazj-auth-token'];
+Object.keys(localStorage).forEach(key => {
+  if (!allowedKeys.includes(key)) {
+    localStorage.removeItem(key);
+  }
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
